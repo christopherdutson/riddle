3d3f08ecf0b264e30979d44bdaa055621dd8a85c
christopherdutson <christopherdutson@gmail.com>
2024-01-07 20:53:36 -0500

    update timer to count up

--- game.js
+++ game.js
@@ -1,29 +1,34 @@
 // Counts down from five minutes, change to count up
 function startTimer() {
-    const endTime = 5 * 60 * 1000 + new Date().getTime();
+    const startTime = new Date().getTime();
     var timerRunning = setInterval(function() {
-        var now = new Date().getTime();
-        var timeLeft = endTime - now;
-    
-        var minutes = Math.floor(timeLeft / (60 * 1000));
-        var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-        document.getElementById("timer").innerHTML = "Timer: " + minutes + ":" + ("0" + seconds).slice(-2);
-        if (timeLeft < 0) {
-            clearInterval(timerRunning);
-            document.getElementById("timer").innerHTML = "Time's up!";
-        }
+        const now = new Date().getTime();
+        const timeUsed = now - startTime;
+        const totalSeconds = Math.floor(timeUsed / 1000);
+        const totalMinutes = Math.floor(totalSeconds / 60);
+        const hours = Math.floor(totalMinutes / 24);
+
+        const secondsStr = ("0" + totalSeconds % 60).slice(-2);
+        const minutesStr = ("0" + totalMinutes % 60).slice(-2);
+        formattedTime = `${hours}:${minutesStr}:${secondsStr}`;
+        
+        document.getElementById("timer").innerText = "Timer: " + formattedTime;
+        // if (timeLeft < 0) {
+        //     clearInterval(timerRunning);
+        //     document.getElementById("timer").innerHTML = "Time's up!";
+        // }
     }, 1000);
 }
 
 // Gets the google sheet (needs to be public) and returns the row data
-async function getSpreadsheet() {
+async function getGoogleSpreadsheet() {
     // me trying to call the app script to get the daily riddle
     // const googleApp = await fetch("https://script.googleapis.com/v1/scripts/AKfycbzizOk-EgTb6-h5BK95cbBYRsCWhDO_U01ytBlAiX2CfqZLW5eHGZaoqyKE6u2pjSBa:run")
     // let data1 = await response.json();
 
     const spreadsheetId = "13mRfFj6yvU6BV_kA4n7hS8m5R5SVR5bJehzcoMtE7Bs";
     // currently my key, change this to Joe's
-    const apiKey = "AIzaSyBEy3OQabheZOWCEAFEpsuk7gl4uMhmydo";
+    const apiKey = "AIzaSyC80r5vgJNtUHT2xxnkX6OylnmmQEVVJ6M";
     const riddleUrl = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}?key=${apiKey}&includeGridData=true`;
     const spreadsheetResponse = await fetch(riddleUrl);
     const spreadsheetData = await spreadsheetResponse.json();
@@ -36,6 +41,11 @@ function dailyRiddleIndex() {
     return 1;
 }
 
+// sanitize string by making lowercase and removing whitespace
+function sanitize(str) {
+    return str.trim().toLowerCase();
+}
+
 function addInputs(parentDiv, answer) {
     var button = document.createElement("input");
     button.setAttribute("type", "submit");
@@ -44,7 +54,7 @@ function addInputs(parentDiv, answer) {
     userInput.setAttribute("type", "text")
     button.addEventListener('click', function() {
         var inputText = userInput.value;
-        if(inputText == answer) { // TODO: sanitize
+        if(sanitize(inputText) == sanitize(answer)) { 
             var icon = document.createElement("i");
             icon.classList.add("fa", "fa-check");
             button.replaceWith(icon);
@@ -54,13 +64,14 @@ function addInputs(parentDiv, answer) {
     parentDiv.appendChild(userInput);
 }
 
-// TODO: add favicon, update puzzle based on day, formatting, timer count up
+// TODO: add favicon, update puzzle based on day, formatting, phone usable, enter should submit
 window.onload = async function() {
-    const spreadsheetRows = await getSpreadsheet();
+    const spreadsheetRows = await getGoogleSpreadsheet();
     const dailyRow = spreadsheetRows[dailyRiddleIndex()].values.map((cell) => cell.formattedValue);
     const titleRow = spreadsheetRows[0].values.map((cell) => cell.formattedValue);
     var mainDiv = document.getElementById("main");
 
+    // Add riddles
     for(let i = 0; i < 4; ++i) {
         const colIndex = 3 * i;
         var rowDiv = document.createElement("div");
@@ -85,6 +96,3 @@ window.onload = async function() {
     
     startTimer();
 }
-
-// https://script.googleapis.com/v1/scripts/AKfycbzizOk-EgTb6-h5BK95cbBYRsCWhDO_U01ytBlAiX2CfqZLW5eHGZaoqyKE6u2pjSBa:run
-// Google Script Deployment ID: AKfycbzizOk-EgTb6-h5BK95cbBYRsCWhDO_U01ytBlAiX2CfqZLW5eHGZaoqyKE6u2pjSBa
